e265f647e21fa7540b9201c89ecdffc2f028ef97
Alexander Stauch <blacke4dawn@gmail.com>
2024-10-13 16:04:32 +0200

    timing control

--- main.py
+++ main.py
@@ -11,11 +11,16 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
+
+    # main game-loop
     while True:
         screen.fill(colours["b"])
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = game_clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
